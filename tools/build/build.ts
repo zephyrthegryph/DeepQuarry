2a73c7686d5400411dec16118dcaa193be18fea8
zephyrthegryph <zephyrthegryph@gmail.com>
2026-08-23 10:57:14 -0500

    Keep map build inputs formatted

--- tools/build/build.ts
+++ tools/build/build.ts
@@ -287,9 +287,9 @@ export const DmTarget = new Juke.Target({
     DreamCheckerTarget, // DQAdd — run SpacemanDMM lint before DM compile if available
   ],
   inputs: [
-		'_maps/map_files/generic/**',
-		'maps/**/*.dm',
-		'maps/**/*.dmm',
+    '_maps/map_files/generic/**',
+    'maps/**/*.dm',
+    'maps/**/*.dmm',
     'code/**',
     'html/**',
     'icons/**',
